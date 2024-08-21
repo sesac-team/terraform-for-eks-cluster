e1b288d894cf2b78a1865f99f3007cd812ef7f54
asusikai <adj0707@gmail.com>
2024-08-21 12:19:17 +0900

    region variables로 변경

--- single-az/network.tf
+++ single-az/network.tf
@@ -3,7 +3,7 @@ module "vpc" {
 
   name = "my-vpc"
   cidr = "10.0.0.0/16"
-  azs             = ["ap-northeast-2a", "ap-northeast-2c"]
+  azs             = ["${var.region}a", "${var.region}c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24", "10.0.4.0/24"]
   public_subnets  = ["10.0.5.0/24", "10.0.6.0/24"]
 
---
+++ single-az/variables.tf
@@ -0,0 +1,5 @@
+variable "region" {
+  description = "The region in which the resources will be created."
+  default     = "ap-northeast-1"
+  
+}
